4cd0612de3cd09c8cb9e88e51baab259e324640b
Ali Kenan Yagmur <akyagmur@gmail.com>
2022-10-09 21:29:14 +0300

    Initial commit

---
+++ src/test/java/com/akyagmur/SWE573/Swe573ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akyagmur.SWE573;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swe573ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
