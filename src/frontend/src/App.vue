39b40f3bad68f0dd06aba9e6180be2e640d45d92
Ali Kenan Yagmur <akyagmur@gmail.com>
2022-11-06 00:12:33 +0300

    feat(#14 #17): install vue js 3 and bootstrap 5
    create login page and home page skeleton

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  width: 100%;
+}
+</style>
