459509b9dfc38c6bd358f66416c96ad8dcfe60d0
Ali Kenan Yağmur <ali.yagmur@ozan.com>
2022-12-11 22:12:13 +0300

    refactor: add for missin key binding for loop

--- src/main/webapp/app/home/Home.vue
+++ src/main/webapp/app/home/Home.vue
@@ -27,7 +27,7 @@
         </div>
       </div>
     </div>
-    <div class="col-md-6" v-for="post in posts">
+    <div class="col-md-6" v-for="post in posts" :key="post">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
           <strong class="d-inline-block mb-2 text-success">Design</strong>
