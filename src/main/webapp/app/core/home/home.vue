e4d29fea53a7eb6c60134854b1e152584734e6d3
Ali Kenan Yağmur <ali.yagmur@ozan.com>
2022-12-11 03:10:23 +0300

    refactor(#14): :sparkles: remove unnecessary js/ts files

--- src/main/webapp/app/core/home/home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-
-  <div>
-    <h1>Welcome to SWE573 app</h1>
-  </div>
-</template>
--- src/main/webapp/app/router/index.js
+++ src/main/webapp/app/router/index.js
@@ -9,10 +9,6 @@ import Router, { RouteConfig } from 'vue-router';
 
 const Home = () => import('@/home/Home.vue');
 const Error = () => import('@/core/error/error.vue');
-import account from '@/router/account';
-import admin from '@/router/admin';
-import entities from '@/router/entities';
-import pages from '@/router/pages';
 
 Vue.use(Router);
 
@@ -36,10 +32,7 @@ const router = new Router({
       name: 'NotFound',
       component: Error,
       meta: { error404: true }
-    },
-    ...account,
-    ...admin,
-    ...pages
+    }
   ]
 });
 
